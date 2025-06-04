88bd29724feb9ecd6887862544b1971f284479d8
l-thatcher <louis@thatchermob.com>
2025-06-04 08:27:33 +0100

    All sorts, dynamic loading of movies from API, search feature started, layout, bg's mapping from API call etc etc

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Search = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-dark-200 font-bold">Search</Text>
+    </View>
+  );
+};
+
+export default Search;
---
+++ app/movies/[id].tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+const MovieDetails = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-dark-200 font-bold">Movie</Text>
+    </View>
+  );
+};
+export default MovieDetails;
